444c85f0c03f5eb54078dc71cf7d2c3a0b154e63
Ryan Lim <ryan9lim@gmail.com>
2017-02-18 17:35:43 -0500

    added tagging methods

--- photo-demo/src/main/java/com/google/android/gms/samples/vision/face/photo/PhotoViewerActivity.java
+++ photo-demo/src/main/java/com/google/android/gms/samples/vision/face/photo/PhotoViewerActivity.java
@@ -15,18 +15,22 @@
  */
 package com.google.android.gms.samples.vision.face.photo;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.util.SparseArray;
 import android.widget.ImageView;
@@ -44,6 +48,9 @@ import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+import static android.os.Environment.DIRECTORY_PICTURES;
+
 /**
  * Demonstrates basic usage of the GMS vision face detector by running face landmark detection on a
  * photo and displaying the photo with associated landmarks in the UI.
@@ -54,6 +61,13 @@ public class PhotoViewerActivity extends Activity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_PHOTO = 1;
 
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     String mCurrentPhotoPath;
     Uri photoURI;
 
@@ -61,8 +75,13 @@ public class PhotoViewerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_viewer);
-        dispatchTakePictureIntent();
+//        verifyStoragePermissions(this);
+        ImageView imageview = (ImageView) findViewById(R.id.test);
+
+        imageview.setImageBitmap(grabImages());
+        Log.d("DSFDSFSDFDS","sDFSDFSDFSFS");
     }
+
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -151,7 +170,7 @@ public class PhotoViewerActivity extends Activity {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = getExternalFilesDir(DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -171,23 +190,29 @@ public class PhotoViewerActivity extends Activity {
         {
             bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, photoURI);
 
-            /*int screenWidth = getResources().getDisplayMetrics().widthPixels;
-            int screenHeight = getResources().getDisplayMetrics().heightPixels;
-            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-                // Notice that width and height are reversed
-                Bitmap scaled = Bitmap.createScaledBitmap(bitmap, screenHeight, screenWidth, true);
-                int w = scaled.getWidth();
-                int h = scaled.getHeight();
-                // Setting post rotate to 90
-                Matrix mtx = new Matrix();
-                mtx.postRotate(90);
-                // Rotating Bitmap
-                bitmap = Bitmap.createBitmap(scaled, 0, 0, w, h, mtx, true);
-            }else{// LANDSCAPE MODE
-                //No need to reverse width and height
-                Bitmap scaled = Bitmap.createScaledBitmap(bitmap, screenWidth,screenHeight , true);
-                bitmap=scaled;
-            }*/
+            ExifInterface ei = new ExifInterface(mCurrentPhotoPath);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+
+            switch(orientation) {
+
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    bitmap = rotateImage(bitmap, 90);
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    bitmap = rotateImage(bitmap, 180);
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    bitmap = rotateImage(bitmap, 270);
+                    break;
+
+                case ExifInterface.ORIENTATION_NORMAL:
+
+                default:
+                    break;
+            }
             return bitmap;
         }
         catch (Exception e)
@@ -196,5 +221,151 @@ public class PhotoViewerActivity extends Activity {
             Log.d(TAG, "Failed to load", e);
             return null;
         }
+    }
+    public Bitmap grabImages()
+    {
+        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .build();
+
+        // This is a temporary workaround for a bug in the face detector with respect to operating
+        // on very small images.  This will be fixed in a future release.  But in the near term, use
+        // of the SafeFaceDetector class will patch the issue.
+        Detector<Face> safeDetector = new SafeFaceDetector(detector);
+
+        for (int i = 1; i < 91; i++){
+            String photoPath = Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS)+"/goodpic"+i+".jpg";
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(photoPath, options);
+            try
+            {
+                ExifInterface ei = new ExifInterface(photoPath);
+                int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                        ExifInterface.ORIENTATION_UNDEFINED);
+
+                switch(orientation) {
+
+                    case ExifInterface.ORIENTATION_ROTATE_90:
+                        bitmap = rotateImage(bitmap, 90);
+                        break;
+
+                    case ExifInterface.ORIENTATION_ROTATE_180:
+                        bitmap = rotateImage(bitmap, 180);
+                        break;
+
+                    case ExifInterface.ORIENTATION_ROTATE_270:
+                        bitmap = rotateImage(bitmap, 270);
+                        break;
+
+                    case ExifInterface.ORIENTATION_NORMAL:
+
+                    default:
+                        break;
+                }
+                // Create a frame from the bitmap and run face detection on the frame.
+                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                SparseArray<Face> faces = safeDetector.detect(frame);
+
+                if (!safeDetector.isOperational()) {
+                    // Note: The first time that an app using face API is installed on a device, GMS will
+                    // download a native library to the device in order to do detection.  Usually this
+                    // completes before the app is run for the first time.  But if that download has not yet
+                    // completed, then the above call will not detect any faces.
+                    //
+                    // isOperational() can be used to check if the required native library is currently
+                    // available.  The detector will automatically become operational once the library
+                    // download completes on device.
+                    Log.w(TAG, "Face detector dependencies are not yet available.");
+
+                    // Check for low storage.  If there is low storage, the native library will not be
+                    // downloaded, so detection will not become operational.
+                    IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+                    boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+                    if (hasLowStorage) {
+                        Toast.makeText(this, R.string.low_storage_error, Toast.LENGTH_LONG).show();
+                        Log.w(TAG, getString(R.string.low_storage_error));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Failed to load", e);
+                return null;
+            }
+        }
+        for (int i = 1; i < 89; i++){
+            String photoPath = Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS)+"/badpic"+i+".jpg";
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(photoPath, options);
+            try
+            {
+                ExifInterface ei = new ExifInterface(photoPath);
+                int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                        ExifInterface.ORIENTATION_UNDEFINED);
+
+                switch(orientation) {
+
+                    case ExifInterface.ORIENTATION_ROTATE_90:
+                        bitmap = rotateImage(bitmap, 90);
+                        break;
+
+                    case ExifInterface.ORIENTATION_ROTATE_180:
+                        bitmap = rotateImage(bitmap, 180);
+                        break;
+
+                    case ExifInterface.ORIENTATION_ROTATE_270:
+                        bitmap = rotateImage(bitmap, 270);
+                        break;
+
+                    case ExifInterface.ORIENTATION_NORMAL:
+
+                    default:
+                        break;
+                }
+                // Create a frame from the bitmap and run face detection on the frame.
+                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                SparseArray<Face> faces = safeDetector.detect(frame);
+
+                if (!safeDetector.isOperational()) {
+                    // Note: The first time that an app using face API is installed on a device, GMS will
+                    // download a native library to the device in order to do detection.  Usually this
+                    // completes before the app is run for the first time.  But if that download has not yet
+                    // completed, then the above call will not detect any faces.
+                    //
+                    // isOperational() can be used to check if the required native library is currently
+                    // available.  The detector will automatically become operational once the library
+                    // download completes on device.
+                    Log.w(TAG, "Face detector dependencies are not yet available.");
+
+                    // Check for low storage.  If there is low storage, the native library will not be
+                    // downloaded, so detection will not become operational.
+                    IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+                    boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+                    if (hasLowStorage) {
+                        Toast.makeText(this, R.string.low_storage_error, Toast.LENGTH_LONG).show();
+                        Log.w(TAG, getString(R.string.low_storage_error));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Failed to load", e);
+                return null;
+            }
+        }
+        safeDetector.release();
+    }
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
     }
 }
